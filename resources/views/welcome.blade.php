98df2dd848f0e48eced3471e36c5ef37258f453c
mahdi jafari <mahdijafari0202@gmail.com>
2021-03-03 08:33:04 +0430

    basics setup and snow background implemented.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,31 +3,178 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
-    <title>Laravel</title>
+    <meta name="application-name" content="Portfolio">
+    <meta name="author" content="mahdi jafari">
+    <meta name="description" content="Mahdi Jafari's Portfolio Website">
+    <meta name="keywords" content="Portfolio, resume, mahdi's website">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{asset('/assets/favicon/apple-touch-icon.png')}}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{asset('/assets/favicon/favicon-32x32.png')}}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{asset('/assets/favicon/favicon-16x16.png')}}">
+    <link rel="manifest" href="/site.webmanifest">
 
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+    <title>mahdi jafari</title>
 
     <!-- Styles -->
-    <style>
-      body {
-        font-family: 'Quicksand-Medium','Nunito';
-        color: #eeeeee;
-      }
-    </style>
+    <link rel="stylesheet" href="{{asset('css/custom.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/font-awesome/css/all.css')}}">
+
     <script  src="{{ asset('/js/app.js')}}"></script>
+    <script  src="{{ asset('/js/custom.js')}}"></script>
   </head>
-<body class="bg-gray-100 dark:bg-gray-800 dark:text-gray-100">
-  <nav class="flex flex-wrap items-center px-10 py-2 bg-gray-900 fixed-top">
-    <a href="/">mahdi jafari</a>
-    <ul class="">
-      <li><a href="" class="text-capitalize">about me</a></li>
-      <li><a href="" class="">my skills</a></li>
-      <li><a href="" class="">my portfolio</a></li>
-      <li><a href="" class="">contact me</a></li>
-    </ul>
-  </nav>
-  
+<body class="bg-gray-100 text-gray-900 dark:bg-gray-900 dark:text-gray-100;">
+    <canvas id="test"></canvas>
+    <div class="w-full root">
+    <nav class="text-gray-900 w-80 dark:text-gray-50">
+        <ul>
+            <li><a href="">somethignc</a></li>
+            <li><a href="">somethignc</a></li>
+            <li><a href="">somethignc</a></li>
+        </ul>
+    </nav>
+
+
+
+
+
+
+
+<div class="text-gray-700 bg-white dark:text-gray-200 dark:bg-gray-900">
+  <div x-data="{ open: false }" class="flex flex-col max-w-screen-xl px-4 mx-auto md:items-center md:justify-between md:flex-row md:px-6 lg:px-8">
+    <div class="flex flex-row items-center justify-between p-4">
+      <a href="/" class="text-lg font-semibold tracking-widest text-gray-900 rounded dark:text-white focus:outline-none focus:shadow-outline">mahdi jafari</a>
+      <button class="rounded md:hidden focus:outline-none focus:shadow-outline" @click="open = !open">
+        <svg fill="currentColor" viewBox="0 0 20 20" class="w-6 h-6">
+          <path x-show="!open" fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z" clip-rule="evenodd"></path>
+          <path x-show="open" fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+        </svg>
+      </button>
+    </div>
+    <nav :class="{'flex': open, 'hidden': !open}" class="flex-col flex-grow hidden pb-4 md:pb-0 md:flex md:justify-end md:flex-row">
+      <a class="px-4 py-2 mt-2 text-sm font-semibold text-gray-900 bg-gray-200 rounded dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:bg-gray-600 dark:focus:text-white dark:hover:text-white dark:text-gray-200 md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="#">About me</a>
+      <a class="px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded dark:bg-transparent dark:hover:bg-gray-600 dark:focus:bg-gray-600 dark:focus:text-white dark:hover:text-white dark:text-gray-200 md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="#">My skills</a>
+      <a class="px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded dark:bg-transparent dark:hover:bg-gray-600 dark:focus:bg-gray-600 dark:focus:text-white dark:hover:text-white dark:text-gray-200 md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="#">My portfolio</a>
+      <a class="px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded dark:bg-transparent dark:hover:bg-gray-600 dark:focus:bg-gray-600 dark:focus:text-white dark:hover:text-white dark:text-gray-200 md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline lg:mr-10" href="#">Contact me</a>
+      <img src="{{asset('assets/images/Mahdi Jafari.jpg')}}" alt="mahdi jafari" class="rounded-full ml-xl-10" width="40px" height="40px">
+    </nav>
+  </div>
+</div>
+  </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
+
+  <script>
+   var w = window.innerWidth,
+    h = window.innerHeight,
+    canvas = document.getElementById('test'),
+    ctx = canvas.getContext('2d'),
+    rate = 60,
+    arc = 100,
+    time,
+    count,
+    size = 5,
+    speed = 6,
+    parts = new Array,
+    colors = ['#ffffff','#ffffff','#ffffff','#ffffff','#ffffff'];
+var mouse = { x: 0, y: 0 };
+
+canvas.setAttribute('width',w);
+canvas.setAttribute('height',h);
+
+function create() {
+  time = 0;
+  count = 0;
+
+  for(var i = 0; i < arc; i++) {
+    parts[i] = {
+      x: Math.ceil(Math.random() * w),
+      y: Math.ceil(Math.random() * h),
+      toX: Math.random() * 5 - 1,
+      toY: Math.random() * 2 - 1,
+      c: colors[Math.floor(Math.random()*colors.length)],
+      size: Math.random() * size
+    }
+  }
+}
+
+function particles() {
+  ctx.clearRect(0,0,w,h);
+   canvas.addEventListener('mousemove', MouseMove, false);
+  for(var i = 0; i < arc; i++) {
+    var li = parts[i];
+    var distanceFactor = DistanceBetween( mouse, parts[i] );
+    var distanceFactor = Math.max( Math.min( 15 - ( distanceFactor / 10 ), 10 ), 1 );
+    ctx.beginPath();
+    ctx.arc(li.x,li.y,li.size*distanceFactor,0,Math.PI*2,false);
+    ctx.fillStyle = li.c;
+    ctx.strokeStyle=li.c;
+    if(i%2==0)
+      ctx.stroke();
+    else
+      ctx.fill();
+
+    li.x = li.x + li.toX * (time * 0.05);
+    li.y = li.y + li.toY * (time * 0.05);
+
+    if(li.x > w){
+       li.x = 0;
+    }
+    if(li.y > h) {
+       li.y = 0;
+    }
+    if(li.x < 0) {
+       li.x = w;
+    }
+    if(li.y < 0) {
+       li.y = h;
+    }
+
+
+  }
+  if(time < speed) {
+    time++;
+  }
+  setTimeout(particles,1000/rate);
+}
+function MouseMove(e) {
+   mouse.x = e.layerX;
+   mouse.y = e.layerY;
+
+   //context.fillRect(e.layerX, e.layerY, 5, 5);
+   //Draw( e.layerX, e.layerY );
+}
+function DistanceBetween(p1,p2) {
+   var dx = p2.x-p1.x;
+   var dy = p2.y-p1.y;
+   return Math.sqrt(dx*dx + dy*dy);
+}
+create();
+particles();
+
+  </script>
+
 </body>
 </html>
